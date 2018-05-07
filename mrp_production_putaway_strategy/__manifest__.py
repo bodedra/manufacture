1823562e96e2b67e78337537e1412ac0420b6282
Lois Rilo <lois.rilo@eficent.com>
2018-05-07 13:28:15 +0200

    [11.0][MIG] mrp_production_putaway_strategy

--- mrp_production_putaway_strategy/__manifest__.py
+++ mrp_production_putaway_strategy/__manifest__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # © 2017 Eficent Business and IT Consulting Services S.L.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
@@ -6,7 +5,7 @@
     "name": "MRP Production Putaway Strategy",
     "summary": "Applies putaway strategies to manufacturing orders for "
                "finished products.",
-    "version": "10.0.1.0.0",
+    "version": "11.0.1.0.0",
     "author": "Eficent, "
               "Odoo Community Association (OCA)",
     "website": "http://www.eficent.com",
